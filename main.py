0d6fe341d0c21e1eb3a76f50b99db47a30b0fa26
Guilherme <144273334+GuilhermeSilva3176@users.noreply.github.com>
2025-07-31 20:13:08 -0300

    Realtime: envia a conversa e recebe a resposta mas tá bugado

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
-from openai_agents.redirect_agent import redirect_message
+from openai_agents.main_agent import session_end, handle_message
 from dotenv import load_dotenv
 from pydantic import BaseModel
+import asyncio
 load_dotenv()
-app = FastAPI()
 
-class MessageRequest(BaseModel):
-    msg: str
 
-@app.post("/agent")
-async def messager_sender(request: MessageRequest):
-    print(request)
-    return await redirect_message(request.msg)
+async def main():
+    try:
+        while True:
+            message = input("User: ")
+            result = await handle_message(message.encode('utf-8'))
+
+            print(f"bot: {result}")
+            
+            if message.lower() == "exit":
+                break
+    except Exception as e:
+        print(f"Error starting session: {e}")
+    finally:
+        await session_end()
+        print("Session ended successfully")
+        
+asyncio.run(main())
